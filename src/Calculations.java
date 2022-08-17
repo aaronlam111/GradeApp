3d5fc2419dbcfae7c397b6b31a87e819dc7cfabf
Aaron <aaronlam111@gmail.com>
2022-08-16 20:47:42 -0700

    Implemented Omit and MarkNeeded

--- src/Calculations.java
+++ src/Calculations.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Calculations {
     static double[] weightPerAssignment;
     static double[] gradePerAssignment;
-    static double totalWeight = 0;
+    static double totalWeight;
 
     public static void weightOfItems(){
         System.out.println("Enter the weight of the assignments/tests in DECIMAL format (press enter after each one):");
@@ -12,42 +12,63 @@ public class Calculations {
         for(int i = 0; i < weightPerAssignment.length; i++){
             weightPerAssignment[i] = s.nextDouble();
         }
-
+        totalWeight = 0;
         for(double i: weightPerAssignment){
             totalWeight += i;
         }
         System.out.println("The total WEIGHT of all your assignments/tests is: " + totalWeight*100 + "%");
     }
 
-    public static void gradeofItems(){
+    public static void gradeOfItems(){
         System.out.println("Enter the GRADE of the assignments/tests in DECIMAL format (press enter after each one):");
         Scanner s = new Scanner(System.in);
         for(int i = 0; i < gradePerAssignment.length; i++){
-            gradePerAssignment[i] = s.nextDouble()*100;
+            gradePerAssignment[i] = s.nextDouble();
         }
     }
 
-    public static double averageCalculation(){
-        double grade = 0;
+    public static void inputGrades() {
         System.out.println("How many assignments/tests do you have a grade for?");
-        int n = GradeApp.fetchInteger(1,99);
+        int n = (int) GradeApp.fetchX(1,99);
         weightPerAssignment = new double[n];
         gradePerAssignment = new double[n];
         weightOfItems();
-//        System.out.println("Is this correct? (Y/N)");
-//        Scanner s = new Scanner(System.in);
-//        String userInput = s.nextLine();
-//        if(userInput == "n") weightOfItems();
-        gradeofItems();
+        gradeOfItems();
+    }
+
+    public static double averageCalculation(){
+        if(weightPerAssignment == null) inputGrades();
+
+        double grade = 0;
         for(int i = 0; i < weightPerAssignment.length; i++){
             grade += weightPerAssignment[i] * gradePerAssignment[i];
         }
         return grade/totalWeight;
     }
 
-    public static void omitCalculation() {
+    public static int omitCalculation() {
+        if(weightPerAssignment == null) inputGrades();
+
+        double max = 0;
+        int indexOfMax = 0;
+        double[] weightedAvg = new double[weightPerAssignment.length];
+        for(int i = 0; i < weightPerAssignment.length; i++){
+            weightedAvg[i] = weightPerAssignment[i] * gradePerAssignment[i];
+            if(weightPerAssignment[i] - weightedAvg[i] > max){
+                max = weightPerAssignment[i] - weightedAvg[i];
+                indexOfMax = i;
+            }
+        }
+        return indexOfMax+1;
     }
 
-    public static void markNeededCalculation() {
+    public static double markNeededCalculation() {
+        if(weightPerAssignment == null) inputGrades();
+
+        System.out.println("What is your desired grade in DECIMAL format?");
+        double desiredGrade = GradeApp.fetchX(0,100);
+        double markNeeded = (desiredGrade - (averageCalculation() * totalWeight))/(1-totalWeight);
+        return markNeeded /*(double) Math.round(markNeeded)*/;
     }
+
 }
--- src/GradeApp.java
+++ src/GradeApp.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 public class GradeApp {
 
-    public static int fetchInteger(int lowerBound, int upperbound){
+    public static double fetchX(int lowerBound, int upperbound){
         Scanner reader;
-        int n = 0;
+        double n = 0;
         while(true) {
             System.out.println("Enter a number:");
             try {
                 reader = new Scanner(System.in);
-                n = reader.nextInt();
-            } catch (InputMismatchException e) {
+                n = reader.nextDouble();
+            } catch (InputMismatchException ignored) {
 
             }
             if(lowerBound <= n && n <= upperbound) {
@@ -24,23 +24,34 @@ public class GradeApp {
     }
 
     public static void main(String args[]){
-        String[] table = new String[5];
-        table[0] = "Welcome to GradeApp!";
-        table[1] = "Please choose what you want to calculate:";
-        table[2] = "1 - Average";
-        table[3] = "2 - What to omit";
-        table[4] = "3 - Mark needed for a certain grade";
-        for(String row : table){
-            System.out.println(row);
-        }
 
-        int userInput = fetchInteger(1, 3);
-        if(userInput == 1){
-            System.out.println("Your grade is " + Calculations.averageCalculation() + "%");
-        }else if(userInput == 2){
-            Calculations.omitCalculation();
-        }else if(userInput == 3){
-            Calculations.markNeededCalculation();
+        System.out.println("Welcome to GradeApp!");
+        while(true) {
+            String[] table = new String[6];
+            table[0] = "Please choose what you want to calculate:";
+            table[1] = "1 - Re-enter marks";
+            table[2] = "2 - Average";
+            table[3] = "3 - What to omit";
+            table[4] = "4 - Mark needed for a certain grade";
+            table[5] = "0 - Exit";
+
+            for (String row : table) {
+                System.out.println(row);
+            }
+
+            int userInput = (int) fetchX(0, 4);
+            if (userInput == 0) {
+                break;
+            } else if (userInput == 1) {
+                Calculations.inputGrades();
+            } else if (userInput == 2) {
+                System.out.println("Your grade is " + Calculations.averageCalculation()*100 + "%");
+            } else if (userInput == 3) {
+                System.out.println("You should omit #" + Calculations.omitCalculation());
+            } else if (userInput == 4) {
+                System.out.println("You need a " + Calculations.markNeededCalculation()*100 + "% " +
+                        "to achieve your desired grade!");
+            }
         }
     }
 }
